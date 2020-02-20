23908936eccb8219b6dbcc0a7cfb3431c0ba3ed8
Bruno <bruno.coelho.santos17@gmail.com>
2020-02-20 17:44:25 -0300

    problema com o user que ta dando pau

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import SessionController from './App/Controllers/SessionController';
 
 const router = new Router();
 
-router.get('/', (req, res) => res.json({ message: 'heloo' }));
+router.post('/login', SessionController.store);
 
 export default router;
